0736f19496039dcba8ecc8223c8b65400d549c15
Bruno Silva <bruno.b.silva@co.sapo.pt>
2017-02-14 15:05:14 +0000

    supporting direct command running such as docker run -rm -it vim

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,11 @@
-set -e
-echo "${USER_PUBLIC_KEY}" > $HOME/.ssh/authorized_keys
+#!/bin/bash
 
-sudo /usr/sbin/sshd -e -D
+set -e
+
+if [ "$1" == '/usr/sbin/sshd' ]; then
+    echo "${USER_PUBLIC_KEY}" > /home/$USER_NAME/.ssh/authorized_keys
+    su-exec root "$@"
+else
+    echo "$@" | su-exec $USER_NAME /bin/bash -i -s
+fi
+
