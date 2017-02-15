2e132c63d234f22bc44d9f7cae01bcc886c4b0bd
Bruno Silva <bruno.b.silva@co.sapo.pt>
2017-02-15 10:46:08 +0000

    dropping interactive bash execution. much faster now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+HOME=/home/$USER_NAME
+
 if [ "$1" == '/usr/sbin/sshd' ]; then
-    echo "${USER_PUBLIC_KEY}" > /home/$USER_NAME/.ssh/authorized_keys
+    echo "${USER_PUBLIC_KEY}" > ~/.ssh/authorized_keys
     su-exec root "$@"
 else
-    su-exec $USER_NAME /bin/bash -i -c "$@"
+    source ~/.bashrc
+    su-exec $USER_NAME "$@"
 fi
 
