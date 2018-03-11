28df7cc5aca225a200340db02d6429c2d6037df4
Bruno Silva <bruno.silva@talkdesk.com>
2018-03-11 12:26:16 +0000

    Removing powerline

--- configuration/.bash.bashrc/prompt.bash
+++ configuration/.bash.bashrc/prompt.bash
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 
-if [ -z "$POWERLINE_LOCATION" ]; then
-    export POWERLINE_LOCATION=`pip show powerline-status | grep Location | sed -e 's/Location: //'`/powerline
-    powerline-daemon -q
-fi
-
-POWERLINE_BASH_CONTINUATION=1
-POWERLINE_BASH_SELECT=1
-source "$POWERLINE_LOCATION/bindings/bash/powerline.sh"
