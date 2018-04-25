c420f8f96a6e248c08bc28031d8d271fda1e9368
Bruno Silva <bruno.silva@talkdesk.com>
2018-04-25 09:29:14 +0100

    Adding some WSL aliases and nativefied apps

--- configuration/.bash.bashrc/wsl.bash
+++ configuration/.bash.bashrc/wsl.bash
@@ -1,3 +1,5 @@
-alias wdocker="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker.exe"
-alias wdocker-compose="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-compose.exe"
+alias docker="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker.exe"
+alias docker-compose="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-compose.exe"
 alias choco="/mnt/c/ProgramData/chocolatey/bin/choco.exe"
+alias welixir="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/elixir.bat"
+alias wmix="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/mix.bat"
