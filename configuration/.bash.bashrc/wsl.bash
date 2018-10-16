f53b0fbedbc86fc4f8baabf394b1e4f0f0af323d
Bruno Silva <b.silva@live.com>
2018-10-16 20:58:36 +0100

    vi instead of vi for git editing
    and some new aliases in wsl

--- configuration/.bash.bashrc/wsl.bash
+++ configuration/.bash.bashrc/wsl.bash
@@ -3,3 +3,5 @@ alias docker-compose="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-c
 alias choco="/mnt/c/ProgramData/chocolatey/bin/choco.exe"
 alias welixir="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/elixir.bat"
 alias wmix="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/mix.bat"
+export GIT_SSH=/mnt/c/ProgramData/chocolatey/bin/PLINK.exe
+alias sbt=cmd.exe /c "C:\Program Files (x86)\sbt\bin\sbt.bat"
