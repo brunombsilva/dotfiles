521f564833ce170691c5198ec9bea6d13253769e
Bruno Silva <b.silva@live.com>
2019-06-10 19:21:34 +0000

    Remove Elixir support

--- configuration/.bash.bashrc/wsl.bash
+++ configuration/.bash.bashrc/wsl.bash
@@ -1,7 +1,5 @@
 alias docker="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker.exe"
 alias docker-compose="/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-compose.exe"
 alias choco="/mnt/c/ProgramData/chocolatey/bin/choco.exe"
-alias welixir="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/elixir.bat"
-alias wmix="cmd.exe /c /mnt/c/ProgramData/chocolatey/lib/Elixir/bin/mix.bat"
 export GIT_SSH=/mnt/c/ProgramData/chocolatey/bin/PLINK.exe
 alias sbt=cmd.exe /c "C:\Program Files (x86)\sbt\bin\sbt.bat"
